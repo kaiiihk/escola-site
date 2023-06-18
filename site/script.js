b1ee877da40418fa584dda8bf240dcf09d891c17
kaik <105812479+kaiiihk@users.noreply.github.com>
2023-06-18 14:10:49 -0300

    Add files via upload

---
+++ site/script.js
@@ -0,0 +1,8 @@
+const images = document.querySelectorAll('.photo-library .image');
+let currentIndex = 0;
+
+setInterval(() => {
+  images[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].classList.add('active');
+}, 3000);
